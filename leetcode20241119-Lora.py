f24d5c21fbdc2d903f8403e4b07fbf13bb2f4268
HuiWLuo <1072655815@qq.com>
2024-11-19 14:53:47 +0800

    Create leetcode20241119-Lora.py

---
+++ leetcode20241119-Lora.py
@@ -0,0 +1,46 @@
+# 47. Permutations II
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        def backtrack(start):
+            if start == len(nums):
+                res.append(nums[:])
+                return
+            
+            for i in range(start, len(nums)):
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start + 1)
+                nums[start], nums[i] = nums[i], nums[start]
+
+        res = []
+        backtrack(0)
+        return res
+
+  # 78. Subsets
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(start, path):
+            result.append(path)
+            for i in range(start, len(nums)):
+                backtrack(i + 1, path + [nums[i]])
+
+        result = []
+        backtrack(0, [])
+        return result
+
+# 77. Combinations
+  class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res=[]
+        def backtrack(start):
+            if start==len(nums):
+                if nums not in res:
+                    res.append(nums[:])
+                return
+            
+            for i in range(start,len(nums)):
+                nums[start],nums[i] = nums[i],nums[start]
+                backtrack(start+1)
+                nums[start],nums[i] = nums[i],nums[start]
+        backtrack(0)
+        return res
