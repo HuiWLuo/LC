5fb48552c5f8d95889494412b29a7693df0ec31c
HuiWLuo <1072655815@qq.com>
2024-11-20 18:20:20 +0800

    Update leetcode20241119-Lora.py

--- leetcode20241119-Lora.py
+++ leetcode20241119-Lora.py
@@ -27,6 +27,18 @@ class Solution:
         backtrack(0, [])
         return result
 
+# 90. Subsets II
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(start, path):
+            result.append(path)
+            for i in range(start, len(nums)):
+                backtrack(i + 1, path + [nums[i]])
+        result = []
+        backtrack(0, [])
+        return result
+
+
 # 77. Combinations
   class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
@@ -43,3 +55,45 @@ class Solution:
                 nums[start],nums[i] = nums[i],nums[start]
         backtrack(0)
         return res
+
+#39. Combination Sum
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def dfs(i, cur, total):
+            if total  == target:
+                res.append(cur.copy())
+                return
+            if i >=len(candidates) or total > target:
+                return
+            
+            cur.append(candidates[i])
+            dfs(i, cur, total + candidates[i])
+            cur.pop()
+            dfs(i+1, cur, total)
+
+        dfs(0, [],0)
+        return res
+
+#40. Combination Sum II
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort() 
+        ans = []
+        n = len(candidates)
+
+        def backtrack(start, target, path):
+            if target == 0:
+                ans.append(path.copy())
+                return
+            if target < 0:
+                return
+            
+            for i in range(start, n):
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+                backtrack(i + 1, target - candidates[i], path + [candidates[i]])
+
+        backtrack(0, target, [])
+        return ans
